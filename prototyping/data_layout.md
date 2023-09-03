deb1fe76f4d0b402651dbe601e93ae2519d2c284
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-09-02 23:07:20 -0700

    Still jittering.

--- prototyping/data_layout.md
+++ prototyping/data_layout.md
@@ -1,78 +1,33 @@
-# SpeakingFaces Data layout
+# Repository Structure and Data Description
 
-FLIR T540 thermal camera (464×348 pixels, 24◦ FOV) and a Logitech C920 Pro HD web-camera (768×512 pixels, 78◦ FOV) with a built-in dual stereo microphone were used for data collection purpose.
+This repository contains annotated data (metadata), raw data, and clean data. The structure of the repository is illustrated in Figure 7a.
 
-Each subject participated in two trials where each trial consisted of two sessions. In the first session, subjects were silent and still, with the operator capturing the visual and thermal video streams through the procession of nine collection angles.
+## Notation
 
-The second session consisted of the subject reading a series of commands as presented one-by-one on the video screens, as the visual, thermal and audio data was collected from the same nine camera positions.
+The names of directories and files follow a specific notation:
 
-Data recording for the first session:
+- `subID = {1 . . . 142}`: Subject number.
+- `trialID = {1, 2}`: Trial number.
+- `sessionID`: 1 if the session does not involve utterances, 2 otherwise.
+- `posID = {1 . . . 9}`: Camera position.
+- `commandID = {1 . . . 1,297}`: Command number.
+- `frameID = {1 . . . 900}`: Number of an image in a sequence.
+- `streamID`: 1 for thermal images, 2 for visual images, and 3 for the aligned version of the visual images.
+- `micID`: 1 for the left microphone and 2 for the right microphone on the web camera.
 
-    Launch the MATLAB and start the global ROS node via MATLAB's terminal: rosinit
-    Open /record_matlab/record_only_video.m and initialize the following parameters:
+## Annotated Data
 
-    sub_id: a subject ID.
-    trial_id: a trial ID.
-    numOfPosit: a total number of positions.
-    numOfFrames: a total number of frames per position.
-    pauseBtwPos: a pause (sec) that is necessary for moving cameras from one position to another.
-    path: a path to save the recorded video files.
+The annotated data are stored in the `metadata` directory, which consists of the `subjects.csv` file and the `commands` subdirectory.
 
-    Launch /record_matlab/record_only_video.m
+- `subjects.csv`: Contains information on the ID, split (train/valid/test), gender, ethnicity, age, and accessories (hat, glasses, etc.) in both trials for each subject.
+- `commands` subdirectory: Consists of `sub_subID_trial_trialID.csv`, composed of records on each command uttered by the subject `subID` in the trial `trialID`. There are 284 files in total, two files for each of the 142 subjects. A record includes the command name, the command identifier, the identifier of a camera position at which the utterance was captured, the transcription of the uttered command, and information on the artifacts detected in the recording.
 
-Data recording for the second session:
+## Raw and Clean Data
 
-    Launch the MATLAB and start the global ROS node via MATLAB's terminal: rosinit
-    Open /record_matlab/record_audio_video.m and initialize the following parameters:
+The raw and clean data are organized in a directory structure that follows the above notation. For example, the path `10/sub_10_ia/trial_1/mic1_audio_cmd_trim/` leads to audio files for subject 10, trial 1, recorded using microphone 1. Similarly, `10/sub_10_ia/trial_1/rgb_image_cmd_aligned/10_1_2_` leads to aligned RGB images for subject 10, trial 1, during session 2.
 
-    sub_id: a subject ID.
-    trial_id: a trial ID.
-    numOfPosit: a total number of positions.
-    fpc: a number of frames necessary for reading one character.
-    pauseBtwPos: a pause (sec) that is necessary for moving cameras from one position to another.
-    path: a path to save the recorded audio and video files.
+Each file name also follows the notation. For instance, `10_1_2_1_1018_1.wav` is an audio file for subject 10, trial 1, session 2, camera position 1, command 1018, version 1. Similarly, `10_1_2_1_1018_10_3.png` is an image file for subject 10, trial 1, session 2, camera position 1, command 1018, frame 10, version 3.
 
-    Launch /record_matlab/record_audio_video.m
+## Frame Rate
 
-### Layout
-
-The schema you provided appears to be a list of audio files in a directory structure. Here's how it can be decoded:
-
-- `10/sub_10_ia/trial_1/mic1_audio_cmd_trim/`: This is the directory path where the audio files are stored. It suggests that these files belong to subject 10, trial 1, and were recorded using microphone 1.
-
-- `10_1_2_1_1018_1.wav`: This is an example of one of the audio files. The filename seems to follow a specific pattern which can be broken down as follows:
-
-    - `10`: This could represent the subject number.
-    - `1`: This could represent the trial number.
-    - `2`: This is unclear without more context, but it could possibly represent a session or task number within the trial.
-    - `1`: This again is unclear without more context, but it could possibly represent a sub-task or activity number within the session.
-    - `1018`: This could be an identifier for the specific command or action performed.
-    - `1`: This could represent a version or take number.
-    - `.wav`: This is the file extension, indicating that this is a WAV audio file.
-
-The new list of files you provided is consistent with the previously decoded schema, but it seems to be a list of image files instead of audio files. Here's how it can be decoded:
-
-- `10/sub_10_ia/trial_1/rgb_image_cmd_aligned/10_1_2_`: This is the directory path where the image files are stored. It suggests that these files belong to subject 10, trial 1, and were captured during session or task 2.
-
-- `10_1_2_1_1018_10_3.png`: This is an example of one of the image files. The filename seems to follow a specific pattern which can be broken down as follows:
-
-    - `10`: This represents the subject number.
-    - `1`: This represents the trial number.
-    - `2`: This could possibly represent a session or task number within the trial.
-    - `1`: This could possibly represent a sub-task or activity number within the session.
-    - `1018`: This could be an identifier for the specific command or action performed.
-    - `10`: This could represent a frame number within the sequence of images captured for this particular action.
-    - `3`: This could represent a version or take number.
-    - `.png`: This is the file extension, indicating that this is a PNG image file.
-    
-### Step 1: Dataset Preparation
-
-We'll use the [SpeakingFaces](https://github.com/IS2AI/SpeakingFaces) dataset, which contains both audio and corresponding face shapes data.
-
-To calculate the total duration of all audio files in a folder, you can use `soxi` with some shell scripting:
-
-```bash
-brew install sox
-sum=0; for file in *.wav; do duration=$(soxi -D "$file"); sum=$(echo $sum + $duration | bc); done; echo $sum
-```
-This script will loop over all `.wav` files in the current directory, get the duration of each file, add this duration to a running total, and then print out the total sum.
+The frame rate for the video data is 28 frames per second.
--- prototyping/make_video.py
+++ prototyping/make_video.py
@@ -14,45 +14,88 @@ locale.setlocale(locale.LC_ALL, '')
 
 from operator import itemgetter
 
+file_structs = []
 class FileStruct:
     def __init__(self):
         self.wave_file = None
         self.png_files = []
         self.command_id = None  # Existing attribute to store the command id
+        self.sub_id = None
+        self.trial_id = None
+        self.session_id = None
+        self.pos_id = None
+        self.frame_id = None
+        self.stream_id = None
+        self.mic_id = None
+        self.mic_num = None 
 
     def add_wave(self, wave_file):
-        cmd_id = os.path.splitext(os.path.basename(wave_file))[0].split('_')[4]  
-        if self.command_id is not None and self.command_id != cmd_id:
-            print(f"Error: Attempted to add wave file with different command id ({cmd_id}) to FileStruct with command id {self.command_id}")
-            self.print()
-            return
-        self.command_id = cmd_id  
-        self.wave_file = wave_file
+        file_parts = os.path.splitext(os.path.basename(wave_file))[0].split('_')
+        sub_id = int(file_parts[0])
+        trial_id = int(file_parts[1])
+        session_id = int(file_parts[2])
+        pos_id = int(file_parts[3])
+        command_id = int(file_parts[4])
+        version = int(file_parts[5])  # Extract version
 
-    def add_png(self, png_file):
-        cmd_id = os.path.splitext(os.path.basename(png_file))[0].split('_')[4]  
-        if self.command_id != cmd_id:
-            print(f"Error: Attempted to add PNG file with different command id ({cmd_id}) to FileStruct with command id {self.command_id}")
-            print(f"PNG file: {png_file}")
+        if self.command_id is not None and (self.sub_id != sub_id or self.trial_id != trial_id or self.session_id != session_id or self.pos_id != pos_id or self.command_id != command_id):
+            print(f"Error: Attempted to add wave file with different attributes to FileStruct")
             self.print()
             return
-        if not any(file[0] == png_file for file in self.png_files):
-            self.png_files.append((png_file, self.wave_file))
+
+        self.sub_id = sub_id
+        self.trial_id = trial_id
+        self.session_id = session_id
+        self.pos_id = pos_id
+        self.command_id = command_id
+        self.version = version
+        self.wave_file = wave_file  # Add this line to actually store the wave file path
+
+    def add_png(self, png_file, file_structs):  # Added file_structs as an argument
+        file_parts = os.path.splitext(os.path.basename(png_file))[0].split('_')
+        sub_id = int(file_parts[0])
+        trial_id = int(file_parts[1])
+        session_id = int(file_parts[2])
+        pos_id = int(file_parts[3])
+        command_id = int(file_parts[4])
+        frame_id = int(file_parts[5])  # Add this line
+        version = int(file_parts[6])  # Add this line
+
+
+        # Find a FileStruct with matching attributes or create a new one
+        file_struct = next((fs for fs in file_structs if fs.sub_id == sub_id and fs.trial_id == trial_id and fs.session_id == session_id and fs.pos_id == pos_id and fs.command_id == command_id and fs.frame_id == frame_id and fs.version == version), None)  # Add frame_id and version to the comparison
+        if file_struct is None:
+            print(f"No matching FileStruct found for PNG file {png_file}")
+            file_struct = FileStruct()
+            file_struct.sub_id = sub_id
+            file_struct.trial_id = trial_id
+            file_struct.session_id = session_id
+            file_struct.pos_id = pos_id
+            file_struct.command_id = command_id
+            file_struct.frame_id = frame_id
+            file_struct.version = version
+            file_structs.append(file_struct)
+
+        if file_struct is not None:
+            file_struct.png_files.append(png_file)
 
     def print(self):
         print(f"Wave file: {self.wave_file}")
-        print("PNG files:")
-        for png_tuple in self.png_files:
-            print(f"PNG file: {png_tuple[0]}, Wave file: {png_tuple[1]}")
+        print(f"PNG files: {self.png_files}")
+        print(f"Command ID: {self.command_id}")
+        print(f"Sub ID: {self.sub_id}")
+        print(f"Trial ID: {self.trial_id}")
+        print(f"Session ID: {self.session_id}")
+        print(f"Pos ID: {self.pos_id}")
+        print(f"Frame ID: {self.frame_id}")
+        print(f"Stream ID: {self.stream_id}")
+        print(f"Mic ID: {self.mic_id}")
+        print(f"Mic Num: {self.mic_num}")
 
 def main(root_dir):
     script_dir = os.path.dirname(os.path.realpath(__file__))
     
-    # Create a list to store FileStruct instances
-    file_structs = []
-
-    last_cmd_number = None  # Store the last command number
-
+    # Processing .wav files
     for dir in glob.glob(os.path.join(root_dir, 'sub_*_ia')):
         if not os.path.isdir(dir):
             continue
@@ -68,74 +111,72 @@ def main(root_dir):
             trial_num = trial_dir.split('_')[-1]
 
             mic_files_1 = natsorted(glob.glob(os.path.join(trial_dir, "mic1_audio_cmd_trim/*.wav")))
-            mic_files_2 = natsorted(glob.glob(os.path.join(trial_dir, "mic2_audio_cmd_trim/*.wav")))
 
-            png_files_rgb = natsorted(glob.glob(os.path.join(trial_dir, "rgb_image_cmd_aligned/*.png")))
-            all_files = mic_files_1 + mic_files_2 + png_files_rgb
-            
+            # Create a dictionary that maps command IDs and versions to .wav files
+            wave_files_by_cmd_id_version = {os.path.splitext(os.path.basename(wave_file))[0].split('_')[4] + '_' + os.path.splitext(os.path.basename(wave_file))[0].split('_')[5]: wave_file for wave_file in mic_files_1}
+
             # Initialize the first FileStruct
             file_struct_rgb = FileStruct()
 
-            # Add the first wave file to the FileStruct
-            if mic_files_1:
-                file_struct_rgb.add_wave(mic_files_1[0])
-            elif mic_files_2:
-                file_struct_rgb.add_wave(mic_files_2[0])
+            # Add wave files to the FileStruct
+            for wave_file in mic_files_1:
+                file_parts = os.path.splitext(os.path.basename(wave_file))[0].split('_')
 
-            for png_file in png_files_rgb:
-                # Extract the command number from the filename
-                cmd_number = int(png_file.split('_')[-4])
-
-                # If the command number is different from the last one, create a new FileStruct
-                if cmd_number != last_cmd_number:
-                    file_struct_rgb = FileStruct()
-                    file_struct_rgb.add_wave(mic_files_1[0] if mic_files_1 else mic_files_2[0])
+                if len(file_parts) > 5:  # Check if there are enough parts before accessing them
+                    cmd_number_version = file_parts[4] + '_' + file_parts[5]  # Use index 5 instead of 6
+                    file_struct_rgb = FileStruct()  # Initialize FileStruct here
+                    file_struct_rgb.add_wave(wave_file)
+                    file_struct_rgb.mic_num = 1  # Set mic_num to 1 for mic_files_1
                     file_structs.append(file_struct_rgb)
+                else:
+                    print(f"Skipping {wave_file} due to insufficient parts")
 
-                file_struct_rgb.add_png(png_file)
-                last_cmd_number = cmd_number  # Update the last command number
+    for dir in glob.glob(os.path.join(root_dir, 'sub_*_ia')):
+        if not os.path.isdir(dir):
+            continue
 
-            file_struct_rgb.print()
+        for trial_dir in glob.glob(os.path.join(dir, 'trial_*')):
+            # Check if trial_dir is actually a directory before listing its contents
+            if os.path.isdir(trial_dir):
+                png_files = glob.glob(os.path.join(trial_dir, 'rgb_image_cmd_aligned/*.png'), recursive=True)
 
-            # Add the processed FileStruct to the list
-            file_structs.append(file_struct_rgb)
+                if not png_files:
+                    print(f"No PNG files found in directory {trial_dir}")
 
-    # Call process_mic_files function here
-    for file_struct in file_structs:
-        mic_file = file_struct.wave_file
-
-        # Raise an exception if mic_file is None
-        if mic_file is None:
-            raise ValueError("No wave file found")
-
-        cmd_id, mic_num = os.path.splitext(os.path.basename(mic_file))[0].split('_')[:2]
-        num_images_per_audio = len(file_struct.png_files)
-
-        # Create a dictionary to store png files by their prefix
-        png_files_by_prefix = {}
-
-        for i in range(num_images_per_audio):
-            png_file, _ = file_struct.png_files[i]  # Access the first element of the tuple
-            prefix = os.path.splitext(os.path.basename(png_file))[0].split('_')[0]
-
-            if prefix not in png_files_by_prefix:
-                png_files_by_prefix[prefix] = []
-
-            png_files_by_prefix[prefix].append(png_file)
-
-        for prefix, png_files in png_files_by_prefix.items():
-            output_file = os.path.join(script_dir, f"{cmd_id}_mic{mic_num}_video_{prefix}.mkv")
-            print(f"Running ffmpeg with output file {output_file}")
-
-            # Create a temporary directory
-            with tempfile.TemporaryDirectory() as temp_dir:
-                # Copy all png files to the temporary directory
                 for png_file in png_files:
-                    shutil.copy2(png_file, temp_dir)
+                    # Extract attributes from the png file
+                    file_parts = os.path.splitext(os.path.basename(png_file))[0].split('_')
+                    sub_id = int(file_parts[0])
+                    trial_id = int(file_parts[1])
+                    session_id = int(file_parts[2])
+                    pos_id = int(file_parts[3])
+                    command_id = int(file_parts[4])
 
-                # Run ffmpeg on the copied png files
-                subprocess.run(['ffmpeg', '-y', '-pattern_type', 'glob', '-i', os.path.join(temp_dir, '*.png'), '-r', '24', '-i', mic_file, '-c:v', 'ffv1', '-level', '3', '-g', '1', '-slices', '24', '-slicecrc', '1', '-c:a', 'flac', '-shortest', output_file])
-        file_struct.print()
+                    file_struct = next((fs for fs in file_structs if fs.sub_id == sub_id and fs.trial_id == trial_id and fs.session_id == session_id and fs.pos_id == pos_id and fs.command_id == command_id), None)
+                    
+                    if file_struct is not None:
+                        # Add the png file to the found FileStruct
+                        file_struct.png_files.append(png_file)
+                    else:
+                        print(f"No matching FileStruct found for PNG file {png_file}")
+
+
+                # Combine wav with png using ffmpeg
+                for file_struct in file_structs:
+                    output_file = os.path.join(script_dir, f"{file_struct.sub_id}_{file_struct.trial_id}_{file_struct.session_id}_{file_struct.pos_id}_{file_struct.command_id}_{file_struct.version}_video.mkv")
+                    print(f"Running ffmpeg with output file {output_file}")
+                    
+                    # Create a temporary directory
+                    with tempfile.TemporaryDirectory() as temp_dir:
+                        # Copy all png files to the temporary directory
+                        for png_file in natsorted(file_struct.png_files):  # Sort the PNG files here
+                            shutil.copy2(png_file, temp_dir)
+                        
+                        # Run ffmpeg on the copied png files
+                        subprocess.run(['ffmpeg', '-y', '-pattern_type', 'glob', '-i', os.path.join(temp_dir, '*.png'), '-r', '28', '-i', file_struct.wave_file, '-c:v', 'ffv1', '-level', '3', '-g', '1', '-slices', '24', '-slicecrc', '1', '-c:a', 'flac', '-shortest', output_file])
+                    
+                    file_struct.print()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some directories.')
