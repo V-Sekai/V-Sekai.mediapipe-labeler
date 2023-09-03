aee47223b859bae092f042aa52eca058338c0cf0
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-09-02 21:20:11 -0700

    Cleanup.

--- prototyping/make_video.py
+++ prototyping/make_video.py
@@ -18,19 +18,19 @@ class FileStruct:
     def __init__(self):
         self.wave_file = None
         self.png_files = []
-        self.command_id = None  # New attribute to store the command id
+        self.command_id = None  # Existing attribute to store the command id
 
     def add_wave(self, wave_file):
-        cmd_id = os.path.splitext(os.path.basename(wave_file))[0].split('_')[4]  # Extract the command id from the filename
+        cmd_id = os.path.splitext(os.path.basename(wave_file))[0].split('_')[4]  
         if self.command_id is not None and self.command_id != cmd_id:
             print(f"Error: Attempted to add wave file with different command id ({cmd_id}) to FileStruct with command id {self.command_id}")
             self.print()
             return
-        self.command_id = cmd_id  # Store the command id
+        self.command_id = cmd_id  
         self.wave_file = wave_file
 
     def add_png(self, png_file):
-        cmd_id = os.path.splitext(os.path.basename(png_file))[0].split('_')[4]  # Extract the command id from the filename
+        cmd_id = os.path.splitext(os.path.basename(png_file))[0].split('_')[4]  
         if self.command_id != cmd_id:
             print(f"Error: Attempted to add PNG file with different command id ({cmd_id}) to FileStruct with command id {self.command_id}")
             print(f"PNG file: {png_file}")
@@ -44,7 +44,6 @@ class FileStruct:
         print("PNG files:")
         for png_tuple in self.png_files:
             print(f"PNG file: {png_tuple[0]}, Wave file: {png_tuple[1]}")
-
 
 def main(root_dir):
     script_dir = os.path.dirname(os.path.realpath(__file__))
@@ -72,22 +71,17 @@ def main(root_dir):
             mic_files_2 = natsorted(glob.glob(os.path.join(trial_dir, "mic2_audio_cmd_trim/*.wav")))
 
             png_files_rgb = natsorted(glob.glob(os.path.join(trial_dir, "rgb_image_cmd_aligned/*.png")))
-            png_files_thr = natsorted(glob.glob(os.path.join(trial_dir, "thr_image_cmd_aligned/*.png")))
-            all_files = mic_files_1 + mic_files_2 + png_files_rgb + png_files_thr
+            all_files = mic_files_1 + mic_files_2 + png_files_rgb
             
             # Initialize the first FileStruct
             file_struct_rgb = FileStruct()
-            file_struct_thr = FileStruct()
 
             # Add the first wave file to the FileStruct
             if mic_files_1:
                 file_struct_rgb.add_wave(mic_files_1[0])
-                file_struct_thr.add_wave(mic_files_1[0])
             elif mic_files_2:
                 file_struct_rgb.add_wave(mic_files_2[0])
-                file_struct_thr.add_wave(mic_files_2[0])
 
-            # Add all png files to the FileStruct
             for png_file in png_files_rgb:
                 # Extract the command number from the filename
                 cmd_number = int(png_file.split('_')[-4])
@@ -95,24 +89,16 @@ def main(root_dir):
                 # If the command number is different from the last one, create a new FileStruct
                 if cmd_number != last_cmd_number:
                     file_struct_rgb = FileStruct()
-                    file_struct_thr = FileStruct()
                     file_struct_rgb.add_wave(mic_files_1[0] if mic_files_1 else mic_files_2[0])
-                    file_struct_thr.add_wave(mic_files_1[0] if mic_files_1 else mic_files_2[0])
                     file_structs.append(file_struct_rgb)
-                    file_structs.append(file_struct_thr)
 
                 file_struct_rgb.add_png(png_file)
                 last_cmd_number = cmd_number  # Update the last command number
 
-            for png_file in png_files_thr:
-                file_struct_thr.add_png(png_file)
-
             file_struct_rgb.print()
-            file_struct_thr.print()
 
             # Add the processed FileStruct to the list
             file_structs.append(file_struct_rgb)
-            file_structs.append(file_struct_thr)
 
     # Call process_mic_files function here
     for file_struct in file_structs:
