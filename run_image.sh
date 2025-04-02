ae44c4b6a405ce84d5b366f2009b2fdfae6107c7
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-04-02 06:54:57 -0700

    Revert "Merge branch 'main' into old"
    This reverts commit 933d869eb4f5d22aff9e9db057c3512e25f5f731, reversing
    changes made to 51466aad4db3521ff7553e8571177db4eb61bc16.

--- run_image.sh
+++
@@ -1,8 +0,0 @@
-# With proper filename quoting and base64 decoding
-media_path="@thirdparty/image.jpg"
-cog predict -i "media_path=$media_path" | \
-tee \
->(jq -r '.debug_media | split(",")[1]' | base64 -d > debug_output.png) \
->(jq -r '.coco_keypoints' > coco_keypoints.json) \
->(jq -r '.blendshapes' > blendshapes.json) \
->/dev/null
--- run_video.sh
+++
@@ -1,7 +0,0 @@
-media_path="@The Best Motion Capture Actress [3_fGRrSKvo0].mp4"
-cog predict -i "media_path=$media_path" | \
-tee \
->(jq -r '.debug_media | split(",")[1]' | base64 -d > debug_output.mp4) \
->(jq -r '.coco_keypoints' > coco_keypoints.json) \
->(jq -r '.blendshapes' > blendshapes.json) \
->/dev/null
